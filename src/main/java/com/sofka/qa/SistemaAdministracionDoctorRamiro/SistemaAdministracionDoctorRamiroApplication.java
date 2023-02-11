a22381b654d85a7a9c4a20127b7dca0e43f03a62
Bryan Polo <david2315@hotmail.com>
2023-02-11 11:30:58 -0500

    Estructura base creada

---
+++ src/main/java/com/sofka/qa/SistemaAdministracionDoctorRamiro/SistemaAdministracionDoctorRamiroApplication.java
@@ -0,0 +1,13 @@
+package com.sofka.qa.SistemaAdministracionDoctorRamiro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaAdministracionDoctorRamiroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaAdministracionDoctorRamiroApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sofka/qa/SistemaAdministracionDoctorRamiro/SistemaAdministracionDoctorRamiroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sofka.qa.SistemaAdministracionDoctorRamiro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaAdministracionDoctorRamiroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
